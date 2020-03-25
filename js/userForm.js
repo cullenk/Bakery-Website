1cfaef0b9b34748b3ce45a4607b34841fd097d32
cullenk <45953462+cullenk@users.noreply.github.com>
2020-03-25 12:27:23 -0400

    final edits

--- js/userForm.js
+++ js/userForm.js
@@ -1,6 +1,6 @@
 /*jshint esversion: 8 */
 
-//Navbar Toggle
+//Navbar
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
@@ -9,11 +9,13 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('display');
 });
 
-//Form Validation
+//Form Prompt
 
-const infoText = document.querySelector('.container');
-const textUpdate = document.querySelector('h5');
-const header = document.querySelector('h3');
+
+
+//Form Validation
+const textUpdate = document.querySelector('.form-text');
+const signUpInfo = document.querySelector('.sign-up-info-container');
 
 function formAnimate() {
   const arrows = document.querySelectorAll('.fa-arrow-down');
@@ -45,39 +47,38 @@ function formAnimate() {
 function validateUser(user) {
   const validation = /^[a-z]{5,}$/;
     if (validation.test(user.value)) {
-      infoText.style.marginTop = '250px';
-      textUpdate.innerText = 'Please enter your email address';
+      textUpdate.innerText = 'Please enter your email address:';
+      textUpdate.classList.remove('sign-up-alert');
       return true;
     } else {
-      // infoText.style.marginTop = '190px';
       textUpdate.innerText = 'Your username should be at least 5 characters long, use only lowercase letters and no numbers.';
-      statusColor('rgb(189,87,87)');
+      textUpdate.classList.add('sign-up-alert');
     }
 }
 
 function validateEmail(email) {
     const validation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (validation.test(email.value)) {
-        textUpdate.innerText = 'Create a password';
+        textUpdate.innerText = 'Create a password:';
+        textUpdate.classList.remove('sign-up-alert');
         return true;
     } else {
         textUpdate.innerText = 'Please enter a valid email address.';
-        statusColor('rgb(189,87,87)');
+        textUpdate.classList.add('sign-up-alert');
     }
 }
 
 function validatePass(password) {
     const validation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
     if (validation.test(password.value)) {
-      // document.body.style.backgroundColor = 'rgb(53, 170, 112)';
-      infoText.style.marginTop = '250px';
-      header.style.opacity = '0';
       textUpdate.innerText = 'Your account has been created!';
+      textUpdate.classList.remove('sign-up-alert');
+      textUpdate.classList.add('sign-up-success');
+      signUpInfo.style.display = 'none';
       return true;
   } else {
-       infoText.style.marginTop = '190px';
        textUpdate.innerText = 'Your password should be at least 8 characters long, have one upper and lowercase letter, and at least one special character.';
-       statusColor('rgb(189,87,87)');
+       textUpdate.classList.add('sign-up-alert');
     }
 }
 
@@ -87,10 +88,4 @@ function nextSlide(parent, nextForm) {
     nextForm.classList.add('active');
 }
 
-function statusColor(color) {
-    textUpdate.style.color = color;
-    window.setTimeout(function() {
-    textUpdate.style.color = 'red';
-  },1200);
-}
 formAnimate();
